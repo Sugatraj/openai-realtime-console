f063cdd0a78f649f87b0b2d2b3b528420ae844fa
Sugatraj <sugatraj.2106@gmail.com>
2025-10-20 13:45:39 +0530

    Add local development script to package.json - Introduce 'dev:local' command for running the server with local configuration.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -171,10 +171,10 @@ export default function App() {
         </div>
       </nav>
       <main className="absolute top-16 left-0 right-0 bottom-0">
-        <section className="absolute top-0 left-0 right-[380px] bottom-0 flex">
-          <section className="absolute top-0 left-0 right-0 bottom-32 px-4 overflow-y-auto">
+        <section className="flex">
+          {/* <section className="absolute top-0 left-0 right-0 bottom-32 px-4 overflow-y-auto">
             <EventLog events={events} />
-          </section>
+          </section> */}
           <section className="absolute h-32 left-0 right-0 bottom-0 p-4">
             <SessionControls
               startSession={startSession}
@@ -186,14 +186,14 @@ export default function App() {
             />
           </section>
         </section>
-        <section className="absolute top-0 w-[380px] right-0 bottom-0 p-4 pt-0 overflow-y-auto">
+        {/* <section className="absolute top-0 w-[380px] right-0 bottom-0 p-4 pt-0 overflow-y-auto">
           <ToolPanel
             sendClientEvent={sendClientEvent}
             sendTextMessage={sendTextMessage}
             events={events}
             isSessionActive={isSessionActive}
           />
-        </section>
+        </section> */}
       </main>
     </>
   );
